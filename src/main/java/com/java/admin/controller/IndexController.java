d9b02942e68641e9ece63a7cdccdefc850fea861
WangJun <133888288326@163.com>
2022-01-19 23:16:58 +0800

    集成mybatis-plus

--- src/main/java/com/java/admin/controller/IndexController.java
+++ src/main/java/com/java/admin/controller/IndexController.java
@@ -1,21 +1,34 @@
 package com.java.admin.controller;
 
+import com.java.admin.entity.User;
+import com.java.admin.mapper.UserMapper;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @Api(tags = "首页模块")
 @RestController
 public class IndexController {
+
+    @Autowired
+    private UserMapper userMapper;
 
     @ApiImplicitParam(name = "name",value = "姓名",required = true)
     @ApiOperation(value = "向客人问好")
     @GetMapping("/sayHi")
     public ResponseEntity<String> sayHi(@RequestParam(value = "name")String name){
+        List<User> users = this.userMapper.selectList(null);
+        for (int i = 0; i < users.size(); i++) {//通过下标遍历数组
+            System.out.println(users.get(i));
+        }
+
         return ResponseEntity.ok("Hi:"+name);
     }
 }
