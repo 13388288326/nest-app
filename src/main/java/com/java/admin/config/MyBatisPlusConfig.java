0271fd60493ead28a7a7741d0aa0dbfdf2c8bdf2
WangJun <133888288326@163.com>
2022-01-21 13:34:31 +0800

    MyBatisPlusConfig配置类

---
+++ src/main/java/com/java/admin/config/MyBatisPlusConfig.java
@@ -0,0 +1,27 @@
+package com.java.admin.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis-plus配置类
+ */
+
+@Configuration
+public class MyBatisPlusConfig {
+    /**
+     * plus分页配置
+     */
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        //分页拦截器
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+        //最大单页限制数量
+        paginationInnerInterceptor.setMaxLimit(1000L);
+        return mybatisPlusInterceptor;
+    }
+}
