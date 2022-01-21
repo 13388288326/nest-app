d28f696069692e9f908ad2d408d08f468d7dc0fd
WangJun <133888288326@163.com>
2022-01-21 15:40:29 +0800

    统一响应处理

---
+++ src/main/java/com/java/admin/utils/ResponseEnum.java
@@ -0,0 +1,25 @@
+package com.java.admin.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * 统一返回枚举
+ * */
+@Getter
+@ToString
+@AllArgsConstructor
+public enum ResponseEnum {
+    SUCCESS(200,"请求成功"),
+    ERROR(500,"服务端错误");
+
+    /**
+     * 响应状态码
+     * */
+    private Integer code;
+    /**
+     * 响应信息
+     * */
+    private String message;
+}
