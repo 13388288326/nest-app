3489dea8b1a19d1a540ae4f2ff134fe083fd8faa
WangJun <133888288326@163.com>
2022-01-28 11:11:05 +0800

    字典编码

---
+++ src/main/java/com/java/admin/entity/DictionaryCode.java
@@ -0,0 +1,36 @@
+package com.java.admin.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@ApiModel("字典编码")
+public class DictionaryCode {
+    @ApiModelProperty("主键")
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    @ApiModelProperty("字典编码")
+    private String code;
+
+    @ApiModelProperty("描述")
+    private String description;
+
+    @ApiModelProperty("排序")
+    private Integer sort;
+
+    @ApiModelProperty("创建时间")
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+
+    @ApiModelProperty("更新时间")
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updateTime;
+}
