63168dca9700a67b40028260549c2c536d42dad9
WangJun <133888288326@163.com>
2022-02-04 21:04:18 +0800

    健康百科service

---
+++ src/main/java/com/java/admin/service/EncyclopediasService.java
@@ -0,0 +1,15 @@
+package com.java.admin.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.java.admin.entity.Encyclopedias;
+
+/**
+ * 健康百科表(Encyclopedias)表服务接口
+ *
+ * @author WangJun
+ * @since 2022-02-04 21:03:53
+ */
+public interface EncyclopediasService extends IService<Encyclopedias> {
+
+}
+
