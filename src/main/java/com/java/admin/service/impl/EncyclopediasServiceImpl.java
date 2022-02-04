ff5dbaf5ddce3e49d781c5af29f37fb4552e5387
WangJun <133888288326@163.com>
2022-02-04 21:08:20 +0800

    健康百科serviceImpl

---
+++ src/main/java/com/java/admin/service/impl/EncyclopediasServiceImpl.java
@@ -0,0 +1,19 @@
+package com.java.admin.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.java.admin.mapper.EncyclopediasMapper;
+import com.java.admin.entity.Encyclopedias;
+import com.java.admin.service.EncyclopediasService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 健康百科表(Encyclopedias)表服务实现类
+ *
+ * @author WangJun
+ * @since 2022-02-04 21:07:55
+ */
+@Service
+public class EncyclopediasServiceImpl extends ServiceImpl<EncyclopediasMapper, Encyclopedias> implements EncyclopediasService {
+
+}
+
